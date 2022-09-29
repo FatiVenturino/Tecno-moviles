200f024a1639a1fead3ddc085e6f937c171eb402
FatiVenturino <53906016+FatiVenturino@users.noreply.github.com>
2022-09-29 12:53:41 -0300

    Add files via upload

---
+++ prototipo1/app/src/main/java/com/iua/prototipo1/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.iua.prototipo1
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View.inflate
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+        private lateinit var binding: ActivityMainBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            Log.d("State working: ","onCreate")
+
+            Toast.makeText(baseContext , "App created" , Toast.LENGTH_LONG).show()
+
+            setTitle("First Android-App")
+
+            binding.button1.setOnClickListener{
+                Toast.makeText(baseContext , "Binding Effect :D" , Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        override fun onStart() {
+            super.onStart()
+            Log.d("State","onStart")
+        }
+
+        override fun onResume() {
+            super.onResume()
+            Log.d("State","onResume")
+        }
+
+        override fun onPause() {
+            super.onPause()
+            Log.d("State", "onPause")
+            Toast.makeText(baseContext , "App on pause" , Toast.LENGTH_LONG).show()
+        }
+
+        override fun onStop() {
+            super.onStop()
+            Log.d("State", "onStop")
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            Log.d("State", "onDestroy")
+        }
+
+    }
+
+class ActivityMainBinding {
+
+
+}
